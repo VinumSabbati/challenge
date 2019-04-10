3b66bed92e90945d3acc8b62186dacddb3fa164d
Matt Scott <matthewruelscott@gmail.com>
2019-04-10 09:34:25 -0400

    Add PropTypes and expected props to LuckyNumberDisplay component

--- src/ui/app/components/LuckyNumberDisplay/LuckyNumberDisplay.js
+++ src/ui/app/components/LuckyNumberDisplay/LuckyNumberDisplay.js
@@ -11,11 +11,14 @@ import PropTypes from 'prop-types';
 class LuckyNumberDisplay extends React.PureComponent {
   render() {
     // TODO: Receive props passed down from LuckyNumber container
-    // const { } = this.props;
+    const { firstName, lastName, luckyNumber } = this.props;
 
     return (
       <div className="mt5 pa4 center w-25 bg-light-gray">
         {/* TODO: Display the user's full name and their lucky number */}
+        Hello, {firstName} {lastName}!
+        <br/>
+        Your lucky number is {luckyNumber}.
       </div>
     );
   }
@@ -23,7 +26,9 @@ class LuckyNumberDisplay extends React.PureComponent {
 
 // TODO: Add PropTypes
 LuckyNumberDisplay.propTypes = {
-
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  luckyNumber: PropTypes.number.isRequired
 };
 
 export default LuckyNumberDisplay;
